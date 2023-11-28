6bfaae765430454ed29691152f576c2231b1099e
mishra_abhishek_kumar <mishraabhishek0807@gmail.com>
2023-11-28 11:59:00 +0530

    Add & Retrieve product to/from DB using sequelizer

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.use(errorController.get404);
 
 sequelize.sync()
     .then(result => {
-        console.log(result);
         app.listen(4000, () => {
             console.log("Listening on PORT: 4000");
         });
--- models/product.js
+++ models/product.js
@@ -5,7 +5,7 @@ const sequelize = require('../util/database');
 const Product = sequelize.define('product', {
     id: {
         type: Sequelize.INTEGER,
-        autoIncrementL: true,
+        autoIncrement: true,
         allowNull: false,
         primaryKey: true
     },
