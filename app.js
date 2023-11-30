4d1c73b826418e6f612dc8c591a8862477d3c2bd
mishra_abhishek_kumar <mishraabhishek0807@gmail.com>
2023-11-30 16:20:01 +0530

    Created User model and managed assosiation

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database')
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+
 
 const app = express();
 
@@ -22,7 +25,10 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync()
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
+sequelize.sync({force: true})
     .then(result => {
         app.listen(4000, () => {
             console.log("Listening on PORT: 4000");
