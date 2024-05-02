f51cedcfcac017c16229d933407f83ab54111b78
mishra_abhishek_kumar <mishraabhishek0807@gmail.com>
2024-05-02 12:10:12 +0530

    integrated mongoDB and able to add new product

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-
+const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
@@ -17,52 +12,26 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findByPk(1)
-    .then(user => {
-        req.user = user;
-        next();
-    })
-    .catch(err => console.log(err));
+    // User.findByPk(1)
+    // .then(user => {
+    //     req.user = user;
+    //     next();
+    // })
+    // .catch(err => console.log(err));
+    next();
 })
 
 app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-//Creating Assosiations
-Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, {through: CartItem});
-Product.belongsToMany(Cart, {through: CartItem});
-
-// sequelize.sync({force: true})
-sequelize.sync()
-    .then(result => {
-        return User.findByPk(1);
-    })
-    .then(user => {
-        if(!user) {
-            return User.create({name: 'Snow', email: 'test@test.com'});
-        }
-        return user;
-    })
-    .then(user => {
-        return user.createCart();
-    })
-    .then(cart => {
-        app.listen(4000, () => {
-            console.log("Listening on PORT: 4000");
-        });
-    })
-    .catch(err => console.log(err));
-
-
+mongoConnect(() => {
+    app.listen(3000);
+})
--- util/database.js
+++ util/database.js
@@ -1,8 +1,30 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'root', { 
-    dialect: 'mysql', 
-    host: 'localhost' 
-});
+let db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+    MongoClient.connect(
+        "mongodb+srv://abhishekmishra02468:pmvOxwZOVVGKJ1VG@cluster0.kpnskqt.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
+    )
+        .then(client => {
+            console.log("Connecte to MongoDB");
+            db = client.db();
+            callback(client);
+        })
+        .catch(err => {
+            console.log(err);
+            throw err;
+        });
+}
+
+const getDb = () => {
+    if(db) {
+        return db;
+    }
+    throw 'No database found';
+}
+
+
+exports.mongoConnect = mongoConnect;
+exports.getDb =  getDb;
