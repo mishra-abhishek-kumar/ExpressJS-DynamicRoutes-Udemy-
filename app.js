cf804ffe0b959b27ea57237257951104d1a20886
mishra_abhishek_kumar <mishraabhishek0807@gmail.com>
2024-05-07 15:00:44 +0530

    integrating mongoose and making product table

--- app.js
+++ app.js
@@ -1,37 +1,45 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+const errorController = require("./controllers/error");
+// const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-    User.findById('66336d671e9f03a908efdf18')
-    .then(user => {
-        req.user = new User(user.name, user.email, user.cart, user._id);
-        next();
-    })
-    .catch(err => console.log(err));
-})
+// app.use((req, res, next) => {
+// 	User.findById("66336d671e9f03a908efdf18")
+// 		.then((user) => {
+// 			req.user = new User(user.name, user.email, user.cart, user._id);
+// 			next();
+// 		})
+// 		.catch((err) => console.log(err));
+// });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-    app.listen(3000);
-})
+mongoose
+	.connect(
+		"mongodb+srv://abhishekmishra02468:59yXBm3rB2RE16Oy@cluster0.kpnskqt.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
+	)
+	.then((result) => {
+		app.listen(3000);
+        console.log("MongoDB connected");
+	})
+	.catch((err) => {
+		console.log(err);
+	});
